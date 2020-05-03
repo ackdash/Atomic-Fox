8db94e2ecb7256c17c991ab230084c90007ed99c
Rob <30823142+ackdash@users.noreply.github.com>
2020-05-03 19:54:55 +1000

    Create Event plumbing

---
+++ Assets/Code/Framework/Events/AtomicEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Framework.Events
+{
+    [CreateAssetMenu]
+    public class AtomicEvent : ScriptableObject
+    {
+        private readonly List<AtomicEventListener> listeners = new List<AtomicEventListener>();
+
+        public void Register(AtomicEventListener listener)
+        {
+            listeners.Add(listener);
+        }
+
+        public void UnRegister<AtomicEventListener>(AtomicEventListener listener)
+        {
+            listeners.RemoveAll(a => a.GetHashCode() == listener.GetHashCode());
+        }
+
+        public void Trigger()
+        {
+            for (var i = 0; i < listeners.Count; i++) listeners[i].OnEventTriggered();
+        }
+    }
+}
---
+++ Assets/Code/Framework/Events/AtomicEventListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Code.Framework.Events
+{
+    public class AtomicEventListener : MonoBehaviour
+    {
+        [SerializeField] [InspectorName("AtomicEvent")]
+        private AtomicEvent atomicEvent;
+
+        [SerializeField] [InspectorName("Target")]
+        private UnityEvent target;
+
+        private void OnEnable()
+        {
+            atomicEvent.Register(this);
+        }
+
+        private void OnDisable()
+        {
+            atomicEvent.UnRegister(this);
+        }
+
+        public void OnEventTriggered()
+        {
+            target.Invoke();
+        }
+    }
+}
