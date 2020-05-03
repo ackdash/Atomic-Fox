90b236beb01eb021b33ff7aa92ec5d5518a14836
Rob <30823142+ackdash@users.noreply.github.com>
2020-05-03 20:04:43 +1000

    Add Editor GUI button to trigger AtomicEventHandlers

---
+++ Assets/Editor/AtomicEventListenerEditor.cs
@@ -0,0 +1,16 @@
+﻿using Code.Framework.Events;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AtomicEventListener))]
+public class AtomicEventListenerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var targetComponent = (AtomicEventListener) target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Trigger Event")) targetComponent.OnEventTriggered();
+    }
+}
