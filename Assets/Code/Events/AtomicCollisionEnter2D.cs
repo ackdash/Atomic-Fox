49f850b015219d5976c73b794f1bf3dac6cf6ca3
Rob <30823142+ackdash@users.noreply.github.com>
2020-05-05 19:48:20 +1000

    Create Atomic Event Collision Enter Component

---
+++ Assets/Code/Events/AtomicCollisionEnter2D.cs
@@ -0,0 +1,16 @@
+﻿using Code.Events.Core;
+using UnityEngine;
+
+namespace Code.Events
+{
+    public class AtomicCollisionEnter2D : MonoBehaviour
+    {
+        [SerializeField] [InspectorName("Event")]
+        private AtomicEvent collisionEvent;
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            collisionEvent.Trigger();
+        }
+    }
+}
