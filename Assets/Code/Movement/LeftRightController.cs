cbd1651fe73791d8251a353e8fffb7638f09a024
Rob <30823142+ackdash@users.noreply.github.com>
2020-05-08 08:54:52 +1000

    Set up speed controller and included keys in input map to control and test speed transitions

---
+++ Assets/Code/Movement/LeftRightController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Movement
+{
+    public class LeftRightController : MonoBehaviour //, ILRMoveable
+    {
+        public int direction;
+
+        [SerializeField] [InspectorName("Speed")]
+        private float speed;
+
+        private SpeedController speedController;
+
+        private void Awake()
+        {
+            speedController = GetComponent<SpeedController>();
+            if (speedController != null) speedController.SetDefaultSpeed(speed);
+        }
+
+
+        public void OnLeft()
+        {
+            direction = Direction.Left;
+        }
+
+        public void OnRight()
+        {
+            direction = Direction.Right;
+        }
+
+        public void OnTurn()
+        {
+            direction *= -1;
+        }
+
+
+        private void Update()
+        {
+            var h = direction * Time.deltaTime;
+            var spd = speedController ? speedController.CurrentSpeed : speed;
+            transform.Translate(Vector2.left * (h * spd), Space.World);
+        }
+
+        private struct Direction
+        {
+            public static int Left { get; } = -1;
+            public static int Right { get; } = 1;
+        }
+    }
+}
---
+++ Assets/Code/Movement/SpeedController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+namespace Code.Movement
+{
+    public class SpeedController : MonoBehaviour
+    {
+        private readonly AnimationCurve curve = AnimationCurve.EaseInOut(0.0f, 0.0f, 2.0f, 2.0f);
+        
+        private float defaultSpeed = 1;
+
+        private bool isAccelerating;
+
+
+        [SerializeField] [InspectorName("SpeedShift")] [Range(0f, 1f)]
+        private float speedShiftFactor = 0.05f;
+
+        private float t;
+        private float targetSpeed;
+
+        public float CurrentSpeed { get; private set; }
+
+        private void Awake()
+        {
+            CurrentSpeed = defaultSpeed;
+            targetSpeed = defaultSpeed;
+        }
+
+        private void Update()
+        {
+            if (isAccelerating)
+            {
+                if (!Mathf.Approximately(targetSpeed, CurrentSpeed))
+                {
+                    t += Time.deltaTime * 0.75f;
+                    CurrentSpeed = Mathf.Lerp(CurrentSpeed, targetSpeed, curve.Evaluate(t));
+                }
+                else
+                {
+                    isAccelerating = false;
+                }
+            }
+        }
+
+        public void SetDefaultSpeed(float speed)
+        {
+            defaultSpeed = speed;
+        }
+
+        public void OnSlowDown()
+        {
+            Accelerate(0.05f);
+        }
+
+        public void OnResetSpeed()
+        {
+            ResetSpeed();
+        }
+        
+        public void OnDecreaseSpeed()
+        {
+            CurrentSpeed *= speedShiftFactor;
+        }
+
+        public void OnIncreaseSpeed()
+        {
+            CurrentSpeed *= 1 / speedShiftFactor;
+        }
+
+        private void Accelerate(float factor)
+        {
+            t = 0;
+            isAccelerating = true;
+            targetSpeed = CurrentSpeed * speedShiftFactor;
+        }
+
+        private void ResetSpeed()
+        {
+            t = 0;
+            isAccelerating = false;
+            CurrentSpeed = defaultSpeed;
+        }
+
+        
+    }
+}
