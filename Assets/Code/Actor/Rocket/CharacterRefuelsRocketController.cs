8570abd246f8a5d6d2796e03a1ac0f8ff8ebe869
Rob <30823142+ackdash@users.noreply.github.com>
2020-06-03 13:59:22 +1000

    Prevenmt refuel after round has ended

--- Assets/Code/Actor/Rocket/CharacterRefuelsRocketController.cs
+++ Assets/Code/Actor/Rocket/CharacterRefuelsRocketController.cs
@@ -6,6 +6,7 @@ namespace Code.Actor.Rocket
 {
     public class CharacterRefuelsRocketController : MonoBehaviour
     {
+        [SerializeField] private bool canFuel;
         public ICollector collector;
         public AtomicEvent fuelCollectedEvent;
         public GameObject fuelReceptor;
@@ -15,11 +16,16 @@ namespace Code.Actor.Rocket
         {
             fuelReceptorId = fuelReceptor.GetInstanceID();
             collector = GetComponent<ICollector>();
+            canFuel = true;
         }
+
+        public void EnableFueling() => canFuel = true;
+
+        public void DisableFueling() => canFuel = false;
 
         public void OnTriggerEnter2D(Collider2D other)
         {
-            if (!collector.HasItems || other.gameObject.GetInstanceID() != fuelReceptorId) return;
+            if (!canFuel || !collector.HasItems || other.gameObject.GetInstanceID() != fuelReceptorId) return;
 
             var fuel = collector.GetItem("Fuel");
 
@@ -28,7 +34,7 @@ namespace Code.Actor.Rocket
             var item = fuel.gameObject.GetComponent<IResetable>();
 
             if (item == null || !other.CompareTag("Fuel Receptor")) return;
-            
+
             fuelCollectedEvent.Trigger();
 
             item.Reset();
